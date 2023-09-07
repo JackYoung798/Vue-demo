9e6aef40d13356598ca6d1de9e7bf6f2b81907b5
Jack <1476280484@qq.com>
2023-09-07 23:07:07 +0800

    commit

--- src/views/goods/goodsList.vue
+++ src/views/goods/goodsList.vue
@@ -7,10 +7,9 @@ import { goodsGetInfoService } from '@/api/goods'
 // })
 
 const tableData = ref([])
-
 const get = async () => {
   const res = await goodsGetInfoService()
-  tableData.value = res.data.data
+  tableData.value = res.data
 }
 get()
 </script>
@@ -20,13 +19,12 @@ get()
     <el-table :data="tableData" height="75vh" border stripe style="width: 100%">
       <el-table-column prop="id" label="商品编号" width="85" />
       <el-table-column prop="name" label="商品名称" width="120" />
-      <el-table-column prop="region" label="类别" width="120" />
+      <el-table-column prop="category" label="类别" width="120" />
       <el-table-column prop="num" label="数量" width="80" />
       <el-table-column prop="price" label="单价" width="80" />
-      <el-table-column prop="date1" label="入库日期" width="160" />
-      <el-table-column prop="date2" label="入库时间" width="160" />
-      <el-table-column prop="condition" label="商品状态" width="100" />
-      <el-table-column prop="desc" label="简要描述" />
+      <el-table-column prop="date" label="入库日期" width="160" />
+      <el-table-column prop="zt" label="商品状态" width="100" />
+      <el-table-column prop="text" label="简要描述" />
     </el-table>
   </page-container>
 </template>
